48c245d08308fa3f287efa87315288a938705c92
Chase (Drift) <chadonsom@gmail.com>
2020-10-28 20:11:42 -0400

    Show an error message if no readme in current directory

--- src/composables.ts
+++ src/composables.ts
@@ -3,6 +3,11 @@
 import { window, workspace, Uri } from 'vscode'
 import { existsSync } from 'fs'
 
+/**
+ * Get the Uri for the readme file in a directory, if it exists
+ * 
+ * @param path path to check for a readme
+ */
 export function readmeForPath(path: string): Uri | undefined {
     let readme, result
     let candidates: Array<string> = ['readme.md', 'README.md']
@@ -16,6 +21,9 @@ export function readmeForPath(path: string): Uri | undefined {
     return readme
 }
 
+/**
+ * Determine the current path, from either the `activeTextEditor`, or the `workspaceFolder`
+ */
 export function currentPath(): string | undefined {
     let current = window.activeTextEditor?.document?.fileName
 
@@ -23,4 +31,12 @@ export function currentPath(): string | undefined {
     else current = workspace.workspaceFolders?.[0]?.uri?.fsPath
 
     return current
+}
+
+/**
+ * Get the parent path of a path (move up)
+ * @param path path to get the parent path of
+ */
+export function parentPath(path: string): string {
+    return path.split('/').slice(0, -1).join('/')
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,50 @@
 /* eslint-disable @typescript-eslint/semi */
 /* eslint-disable curly */
-import * as vscode from 'vscode'
+import { window, commands, ExtensionContext } from 'vscode'
 import { currentPath, readmeForPath } from './composables'
 
-export async function activate(context: vscode.ExtensionContext) {
+export async function activate(context: ExtensionContext) {
 
 	// Preview the readme file
-	let view = vscode.commands.registerCommand('auto-view-readme.view', async () => {
+	let view = commands.registerCommand('auto-view-readme.view', async () => {
 		let current = currentPath()
 		if (!current) return console.error("No folder or file open.")
 
 		// Open readme candidate
 		let readme = readmeForPath(current)
-		if (!readme) return console.error("No readme found in current directory.")
-		await vscode.commands.executeCommand('vscode.open', readme)
-		/**
-		 * Upon getting it installed on code-server:
-		 * Command 'Auto-View-Readme: View' resulted in an error 
-		 * (Running the contributed command: 'vscode.open' failed. Illegal argument 'resource' - Resource to open)
-		 */
+		if (!readme) return window.showErrorMessage("No readme found in the current directory.")
+		await commands.executeCommand('vscode.open', readme)
+		
 		// Show preview
-		await vscode.commands.executeCommand('markdown.showPreview')
+		await commands.executeCommand('markdown.showPreview')
 
 		// Remove editor for readme candidate
 		await new Promise(resolve => setTimeout(resolve, 10))
-		await vscode.commands.executeCommand('workbench.action.previousEditor')
+		await commands.executeCommand('workbench.action.previousEditor')
 		await new Promise(resolve => setTimeout(resolve, 10))
-		await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+		await commands.executeCommand('workbench.action.closeActiveEditor')
 	})
 
 	// Open the readme file only, no preview
-	let open = vscode.commands.registerCommand('auto-view-readme.open', async () => {
+	let open = commands.registerCommand('auto-view-readme.open', async () => {
 		let current = currentPath()
 		if (!current) return console.error("No folder or file open.")
 
 		// Open readme candidate
 		let readme = readmeForPath(current)
-		await vscode.commands.executeCommand('vscode.open', readme)
+		await commands.executeCommand('vscode.open', readme)
 	})
 
 	// Open the readme file with the preview to the side of the editor
-	let openWithPreview = vscode.commands.registerCommand('auto-view-readme.openWithPreview', async () => {
+	let openWithPreview = commands.registerCommand('auto-view-readme.openWithPreview', async () => {
 		let current = currentPath()
 		if (!current) return console.error("No folder or file open.")
 
 		// Open readme candidate
 		let readme = readmeForPath(current)
-		await vscode.commands.executeCommand('vscode.open', readme)
+		await commands.executeCommand('vscode.open', readme)
 		// Show preview to side
-		await vscode.commands.executeCommand('markdown.showPreviewToSide')
+		await commands.executeCommand('markdown.showPreviewToSide')
 	})
 
 	context.subscriptions.push(view, open, openWithPreview)
